d5e9709ffd8caef1104cd730c0a712b187f2897a
Jay Telgote <120390468+ijaytelgote@users.noreply.github.com>
2024-03-17 17:32:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from flask import Flask, jsonify
-import os
+from GeneratedData import GeneratedData
+from Visualization import VisualizationDashboard
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+if __name__ == "__main__":
+    data = GeneratedData.generate_fake_data(rows=100)
+    dashboard = VisualizationDashboard(data)
+    dashboard.run_server()
