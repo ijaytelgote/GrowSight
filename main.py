a3a86d4eff0cf17af6229b3bb7b8d19344ec20ee
Jay Telgote <120390468+ijaytelgote@users.noreply.github.com>
2024-03-17 17:38:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from Visualization import VisualizationDashboard
 if __name__ == "__main__":
     data = GeneratedData.generate_fake_data(rows=100)
     dashboard = VisualizationDashboard(data)
+    app = dashboard.app  # Accessing the Dash app instance from VisualizationDashboard
+    app.layout = dashboard.default_layout  # Set the layout
     dashboard.run_server()
