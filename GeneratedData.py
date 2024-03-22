b76f2f29147899645148705015938b0ad44ddf04
Jay Telgote <120390468+ijaytelgote@users.noreply.github.com>
2024-03-22 18:20:56 +0530

    Update GeneratedData.py

--- GeneratedData.py
+++ GeneratedData.py
@@ -1,5 +1,4 @@
 import random
-import numpy as np
 import pandas as pd
 from faker import Faker
 
@@ -10,10 +9,16 @@ class DataGenerator:
     def generate_fake_data(rows=100):
         data = []
 
-        for _ in range(rows):
+        # Generating date ranges outside the loop
+        last_email_sent_dates = pd.date_range(start='2023-01-01', periods=rows, freq='M').strftime('%B %Y')
+        last_interaction_dates = pd.date_range(start='2020-10-01', periods=rows, freq='M').strftime('%B %Y')
+        last_meeting_dates = pd.date_range(start='2022-12-01', periods=rows, freq='M').strftime('%B %Y')
+        last_phone_call_dates = pd.date_range(start='2003-01-06', periods=rows, freq='M').strftime('%B %Y')
+
+        for i in range(rows):
             row = {
-                 'Timestamp': fake.date_time_between(start_date='-30d', end_date='now'),
-                 'comment': fake.text(),
+                'Timestamp': fake.date_time_between(start_date='-30d', end_date='now'),
+                'comment': fake.text(),
                 "Monthly Revenue": random.randint(1000, 10000),
                 "Opportunity Amount": random.randint(10000, 100000),
                 "Support Tickets Open": random.randint(0, 10),
@@ -29,13 +34,14 @@ class DataGenerator:
                 "Population": random.randint(1000000, 1000000000),
                 "Area (sq km)": random.randint(100000, 10000000),
                 "GDP (USD)": random.randint(1000000, 10000000000),
-                'Last Email Sent Date': (pd.date_range(start='2023-01-01', periods=rows, freq='M')).strftime('%B %Y'),
-                'Last Interaction Date': (pd.date_range(start='2020-10-01', periods=rows, freq='M')).strftime('%B %Y'),
-                'Last Meeting Date': (pd.date_range(start='2022-12-01', periods=rows, freq='M')).strftime('%B %Y'),
-                'Last Phone Call Date': (pd.date_range(start='2003-01-06', periods=rows, freq='M')).strftime('%B %Y'),
+                'Last Email Sent Date': last_email_sent_dates[i],
+                'Last Interaction Date': last_interaction_dates[i],
+                'Last Meeting Date': last_meeting_dates[i],
+                'Last Phone Call Date': last_phone_call_dates[i],
                 'Probability of Close': random.randint(0, 100)
             }
             data.append(row)
 
         df = pd.DataFrame(data)
         return df
+DataGenerator.generate_fake_data()
