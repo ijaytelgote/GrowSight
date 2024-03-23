58beddfcf2ef58bb8026eacfe6c0698ad24a3b2a
Jay Telgote <120390468+ijaytelgote@users.noreply.github.com>
2024-03-23 13:01:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -173,7 +173,11 @@ class VisualizationDashboard:
         ], style={'textAlign': 'center'})
 
 # Initialize the Dash app
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app = dash.Dash(__name__,suppress_callback_exceptions=True,
+                 meta_tags=[{'name': 'viewport',
+                             'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]
+                )
+
 dashboard = VisualizationDashboard()
 
 
