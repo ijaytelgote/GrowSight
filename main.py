ab29f4d81c270e8589308a9535347f219923b6fe
Jay Telgote <120390468+ijaytelgote@users.noreply.github.com>
2024-03-27 19:21:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -388,17 +388,18 @@ def update_graph(start_date, end_date):
     # Create plot
     trace = go.Scatter(
         x=filtered_df['Timestamp'],
-        y=filtered_df['Sentiment'].apply(lambda x: 0 if x == 'Neutral' else (0.5 if x == 'Happy' else -0.5)),  # Adjusting y-values to bring data points closer
+        y=filtered_df['Sentiment'].apply(lambda x: 1 if x == 'Happy' else (-1 if x == 'Sad' else 0)),
         mode='lines+markers',
         name='Sentiment',
-        marker=dict(color=['blue' if x == 'Neutral' else ('green' if x == 'Happy' else 'red') for x in filtered_df['Sentiment']])  # Adjusting marker colors accordingly
+        marker=dict(color=['green' if x == 'Happy' else ('red' if x == 'Sad' else 'blue') for x in filtered_df['Sentiment']])
     )
 
     layout = go.Layout(
+     
         xaxis=dict(title='Timestamp', showgrid=False, zeroline=False),
-        yaxis=dict(tickvals=[-0.5, 0, 0.5], ticktext=['Sad', 'Neutral', 'Happy'], showgrid=False, zeroline=False),
-        margin=dict(l=55, r=1, t=1, b=1)
-    )
+        yaxis=dict( tickvals=[-1, 0, 1], ticktext=['Sad', 'Neutral', 'Happy'],showgrid=False, zeroline=False),
+                       margin=dict(l=55, r=1, t=1, b=1))
+    
 
     return {'data': [trace], 'layout': layout}
 
